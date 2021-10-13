ec580d56bc408386443d32475ff61b104f921c49
Pallav <pallavinvizag@gmail.com>
2021-10-13 22:57:32 +0530

    new event schema added and user.js modified

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,9 @@
 import userModel from './user.js';
 import tokenModel from './token.js';
 import logModel from './log.js';
+import eventModel from './event.js'
 
 export const User = userModel;
 export const Token = tokenModel;
 export const Log = logModel;
+export const Event = eventModel;
