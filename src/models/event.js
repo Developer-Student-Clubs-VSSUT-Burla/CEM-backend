4ffae61d39244fd97a59eb39e3d6739d03200d4f
Pallav <pallavinvizag@gmail.com>
2021-10-14 21:47:09 +0530

    Documentation for events added

--- src/models/event.js
+++ src/models/event.js
@@ -1,45 +1,64 @@
+import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
-const eventSchema = new Schema({
-    Name:{
-        type: String,
-        required: true,
+const eventSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    Date:{
-        type: Date,
-        required: true,
-
+    eventDate: {
+      type: Date,
+      required: true,
     },
-    Type:{
-        type:String,
-        enum:["online","offline"],
-        required:true,
+    eventType: {
+      type: String,
+      enum: ["online", "offline"],
+      required: true,
     },
-    RegistrationFee:{
-        type:Number,
-        required:true
-
+    registrationFee: {
+      type: Number,
+      required: true,
     },
-    ExpectedAttendees:{
-        type:Number,
-        required:true
+    expectedAttendees: {
+      type: Number,
+      required: true,
     },
-
-    CreatedBy:{
-      type:Schema.Types.ObjectId,
-      ref:"User"
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-
-    CreatedOn:{
-        type:Date,
-        required:true,
-        
-    }
-
-},
-{
+    createdOn: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
     timestamps: true,
-}
+  }
 );
- const Event = model("Event",eventSchema);
- export default Event;
+const Event = model("Event", eventSchema);
+export default Event;
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Event:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *         eventDate:
+ *           type: date
+ *         eventType:
+ *           type: string
+ *           enum: ["online", "offline"]
+ *         registrationFee:
+ *           type: number
+ *         expectedAttendees:
+ *           type: number
+ *         createdBy:
+ *           type: Schema.Types.ObjectId
+ *         createdOn:
+ *           type: date
+ */
